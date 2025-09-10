fa6352b5221111b63e781f797e9034ef2bb1de18
frane <frane.kuna@gmail.com>
2025-09-10 11:37:38 +0200

    DbContext

---
+++ TodoAPI.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoAPI.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ITaskRepository Tasks { get; }
+        IUserRepository Users { get; }
+        ICategoryRepository Categories { get; }
+
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
