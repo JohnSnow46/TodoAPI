b920daceaf63b9fa842b9c9fe784b3d1a59545c4
frane <frane.kuna@gmail.com>
2025-09-11 11:59:54 +0200

    jwt

---
+++ TodoAPI.Core/Interfaces/IJwtService.cs
@@ -0,0 +1,14 @@
+﻿using TodoAPI.Core.Entities;
+using TodoAPI.Application.DTOs.common;
+
+namespace TodoAPI.Application.Interfaces
+{
+    public interface IJwtService
+    {
+        string GenerateToken(User user);
+        string GenerateRefreshToken();
+        AuthResponse GenerateAuthResponse(User user);
+        bool ValidateToken(string token);
+        Guid? GetUserIdFromToken(string token);
+    }
+}
